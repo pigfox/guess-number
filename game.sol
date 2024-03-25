112d898d3875862c21cea8087e5f474bc3e3e2dd
Peter Sjolin <pigfoxeria@gmail.com>
2024-03-24 22:59:03 -0700

    first commit

---
+++ game.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract GuessNumber {
+    address private owner;
+    uint private guessedNumber;
+    uint public actualNumber;
+    uint private maxNumber;
+    mapping(address => uint) private wins;
+
+    constructor() {
+        owner = msg.sender;
+        maxNumber = 10; // Default value to ensure it's > 0
+        newGame();
+    }
+
+    function guess(uint _guessedNumber) public returns (bool) {
+        if (actualNumber == _guessedNumber) {
+            newGame();
+            wins[msg.sender]++;
+            return true;
+        }
+        return false;
+    }
+
+    function newGame() public {
+        actualNumber = generateRandomNumber();
+    }
+
+    function setMaxNumber(uint _maxNumber) public {
+        require(0 < _maxNumber, "Max number must be greater thatn zero");
+        maxNumber = _maxNumber;
+    }
+
+    function getMaxNumber() public view returns (uint) {
+        return maxNumber;
+    }
+
+    function getWins() public view returns (uint) {
+        return wins[msg.sender];
+    }
+
+    function generateRandomNumber() private view returns (uint) {
+        uint randomHash = uint(
+            keccak256(abi.encodePacked(block.timestamp, msg.sender))
+        );
+        return uint(randomHash % maxNumber);
+    }
+}
